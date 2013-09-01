8fe4692427bdee6535ac96828955ae89afb42f5a
Rafal <rafal@Norma.local>
2013-09-01 21:10:17 +0100

    With two plots overlayed—not quite right, though.

--- plots.R
+++ plots.R
@@ -1,16 +1,32 @@
 # Plot film test characteristic curves and calculate Kodak-style CI (Contrast Index)
-# (c) Rafal Lukawiecki photo@rafal.net 2013-08-21
+ 
+# Copyright 2013 (c) Rafal Lukawiecki photo@rafal.net
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details at:
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
+# Version 0.2 2013-09-01
+
+# **** START HERE:
 # Currently, the algorithm fails if it starts the search for a solution at a "wrong" starting point.
 # If CI values are not being computed (showing as NA), change the below starting point to a set of
 # different rel log E exposure values.
 starting.point = c(0.95, 1, 2)
 
+# **** HOW TO USE:
 # To use, call as below, providing your densitometer readings in the first parameter, as data frame, which must contain as the first
 # column, named "He" your relative log E values (from your sensitometer etc), starting at 0 and increasing towards the right.
 # Every remaining column should contain density readings. Readings can be absolute (ie. including FB+F) or relative, the code below
-# will subtract FB+F from each column automatically. Bear in mind this may influence your choice of "offset" if used.
-# Name those columns in a way that describes your test, eg. "11 min" etc.
+# will subtract the smallest reading, assuming it is FB+F from each column, respectively, automatically.
+# Make sure to name your columns something like "7 min" etc. If you do, and there are at least 2 columns, this program will attempt to plot
+# a Dev Time/CI curve. It will also show, as lines on that plot, the target CIs for the different N-values.
 # If your data is in a CSV that contains column headers (the first must be called He), or another text file, 
 # use read.csv (or another read.table function) to put it in a data frame, for example:
 # delta100.ddx1.4 <- read.csv("delta100.ddx1.4.csv")
@@ -19,16 +35,17 @@ starting.point = c(0.95, 1, 2)
 # in the corresponding column represents exposures greater by the value of the offset. This is useful if you want to take a second
 # sensitometric exposure which is larger, by that offset, to effectively compute a longer curve, without using a longer step tablet.
 
-# plot.film.test(delta100.ddx1.4, "Delta 100 4x5 DDX 1+4 20˚C CombiPlan 30s/3inv.5s@30s", "Exposure\nEseco SL-2\nGreen x2 + x6", 
-#               log.e.offset=c(0,.21,0,.21,0,.21,0,.21,0,.21), df=7)
+# plot.film.test(delta100.ddx1.4) # This is the simplest way to use it. Or make it more complex:
+# plot.film.etest(delta100.ddx1.4, "Delta 100 4x5 DDX 1+4 20˚C CombiPlan 30s/3inv.5s@30s", "Exposure\nEseco SL-2\nGreen x2 + x6", 
+#               log.e.offset=c(0,.21,0,.21,0,.21,0,.21,0,.21), df=7, dev.time.smoothing=2)
 
-# This is the main function, that plots the curves and computes the CIs
+# This is the main function, that plots the curves, computes the CIs, and plots the Dev/CI chart (if enough data was provided)
 plot.film.test <- function(film.data, # data frame as described above
                            title = "HD Film Plot", # Title for the plot
                            sensitometry = "Tests",   # Title for the legend
-                           target.N.CIs=c(0.4, 0.5, 0.58, 0.7, 0.88), # Target CIs, will be drawn on CI/Dev plot and annotated as N-2,...
+                           target.N.CIs=c(0.4, 0.5, 0.58, 0.7, 0.88), # Target CIs, will be drawn on CI/Dev plot and annotated as N-2,...,N+2
                            log.e.offset = NULL,  # Exposure offset vector, see above
-                           df=5,  # Degrees of freedom for controlling how smooth (less) or fitted the Bezier spline ought to be
+                           df=6,  # Degrees of freedom for controlling how smooth (less) or fitted the Bezier spline ought to be
                            dev.time.smoothing=2 # Order of the smoothing polynomial for the development time/CI chart (use 1 for straight line)
                            )
   {  
@@ -36,7 +53,7 @@ plot.film.test <- function(film.data, # data frame as described above
   require(ggplot2)
   require(splines)
   
-  # Assume that the lowest recorded reading in each column is FB+F, substract it, column-wise, (He columns should starts at 0)
+  # Assume that the lowest recorded reading in each column is FB+F, and subtract it column-wise, (He column should start at 0)
   film.data <- as.data.frame(apply(film.data, 2, function (x) x-min(x[1])))
   
   # If no log exposure offset vector provided, assume 0 offsets
@@ -91,8 +108,6 @@ plot.film.test <- function(film.data, # data frame as described above
                                  labels = paste(names(film.data[2:ncol(film.data)]), CIs, sep=" CI=")) +
     ggtitle(title)
   
-  print(p)
-  
   # If the columns represent development times, prepare a data frame of those times and corresponding CIs for a CI/dev plot.
   # If the titles cannot be parsed as having numerical meaning, print a warning, and plot nothing.
   dev.time <- sapply(names(film.data)[-1], function (x) as.numeric(unlist(strsplit(x, "[ \\.X\\-]"))[1]))
@@ -110,11 +125,34 @@ plot.film.test <- function(film.data, # data frame as described above
         geom_hline(yintercept=target.N.CIs, alpha=0.6, colour="blue", linetype="dashed") + 
         annotate("text", x=max(dev.time), y=target.N.CIs + 0.02, 
                  label=paste(c('N-2', 'N-1', 'N', 'N+1', 'N+2'), "(CI", target.N.CIs, ")"), size=3) +
+        + opts(legend.position = "none") 
         ggtitle(paste(title, "Development Time/CI Chart", sep="\n"))
-        print(dp)
+      
+      require(grid)
+      require(gtable)
+      
+      g1 <- ggplot_gtable(ggplot_build(p))
+      g2 <- ggplot_gtable(ggplot_build(dp))
+      
+      # overlap the panel of 2nd plot on that of 1st plot
+      pp <- c(subset(g1$layout, name == "panel", se = t:r))
+      g <- gtable_add_grob(g1, g2$grobs[[which(g2$layout$name == "panel")]], pp$t, 
+                           pp$l, pp$b, pp$l)
+      
+      ## what I dd is to plot a new viewports.
+      vp=viewport(x=0.25,y=1,height=0.5,width=0.5)
+      pushViewport(vp)
+      grid.draw(g)
+      upViewport()
+      
+      
+      #  print(dp)
+      return()
     }
   }
-
+  
+  print(p)
+  
 }
 
 
