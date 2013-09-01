63fbb68b0468d514a566d0f2dd52de37a9171f77
Rafal <rafal@Norma.local>
2013-09-01 01:19:10 +0100

    Tidied up, seems to work, but starting point is a consideration...

--- plots.R
+++ plots.R
@@ -1,48 +1,30 @@
 # Plot film test characteristic curves and calculate Kodak-style CI (Contrast Index)
 # Rafal Lukawiecki photo@rafal.net 2013-08-21
 
-Dd <- function(x, curve.model, log.e.offset=0) 
-  predict(curve.model, data.frame(He=c(x-log.e.offset)))[1]
+# Currently, the algorithm fails if it starts the search for a solution at a "wrong" starting point.
+# If CI values are not being computed (showing as NA), change the below starting point to a set of
+# different rel log E exposure values.
+starting.point = c(0.9, 1, 2)
+
+# To use, call as below, providing your densitometer readings in the first parameter, as data frame, which must contain as the first
+# column, named "He" your relative log E values (from your sensitometer etc), starting at 0 and increasing towards the right.
+# Every remaining column should contain density readings. Name those columns in a way that describes your test, eg. "11 min" etc.
+# Optionally, provide a vector of exposure offsets as the log.e.offset parameter. This is used to indicate that a series of readings
+# in the corresponding column represents exposures greater by the value of the offset. This is useful if you want to take a second
+# sensitometric exposure which is larger, by that offset, to effectively compute a longer curve, without using a longer step tablet.
+
+plot.film.test(min.delta, "Delta 100 4x5 XTOL 1:1 20˚C 10min Tray", "Exposure\nEseco SL-2", log.e.offset=c(0.21,0,0), df=7)
 
 
-ci.equation <- function(x, curve.model, log.e.offset = 0) {
-  
-  D <- function(x) 
-    predict(curve.model, data.frame(He=c(x-log.e.offset)))[1]
-  
-  c(
-    F1 = (x[2] - x[1])^2 + D(x[2])^2 - 0.2^2,
-    F2 = (x[3] - x[2])^2 + (D(x[3]) - D(x[2]))^2 - 2^2,
-    F3 = (x[3] - x[1])^2 + D(x[3])^2 - 2.2^2
-  )
-  
-}
 
-
-contrast.index <- function(curve.model, log.e.offset = 0) {
-  
-  require(rootSolve)
-  
-  roots <- multiroot(f=ci.equation, start=c(0.85, 1, 2.63), 
-                     positive=TRUE, maxiter=1000, ctol=0.05, curve.model=curve.model, log.e.offset=log.e.offset)
-  CI.points <- data.frame(He=c(roots$root[2:3]) - log.e.offset)
-  CI.points$D <- predict(curve.model, CI.points[1])
-  
-  print(roots)
-  print(CI.points)
-  
-  return(
-    (CI.points$D[2] - CI.points$D[1]) / (CI.points$He[2] - CI.points$He[1])
-  )
-  
-}
-
-
-plot.film.test <- function(film.data, 
-                           title = "HD Film Plot", 
-                           sensitometry = "Tests", 
-                           log.e.offset = NULL,
-                           df=5) {
+# This is the main function, that plots the curves and computes the CIs
+plot.film.test <- function(film.data, # data frame as described above
+                           title = "HD Film Plot", # Title for the plot
+                           sensitometry = "Tests",   # Title for the legend
+                           log.e.offset = NULL,  # Exposure offset vector, see above
+                           df=5  # Degrees of freedom for controlling how smooth (less) or fitted the Bezier spline ought to be
+                           )
+  {  
   
   require(ggplot2)
   require(splines)
@@ -70,13 +52,16 @@ plot.film.test <- function(film.data,
     test.name <- names(film.data[test])
     rel.log.e <- paste("He + ", log.e.offset[test-1])
     
-    # Find and fit a smoothing linr
+    # Find and fit a smoothing line
     #smooth.fit <- loess(film.data[,test] ~ He, film.data, span=0.75)   # LOESS variant
     smooth.fit <- lm(film.data[,test] ~ bs(He+log.e.offset[test-1], df = df), data = film.data)  # Bezier splines, note the default degrees of freedom
     smooth.curves <- cbind(smooth.curves, predict(smooth.fit, smooth.curves[1]))
     
     # Calculate CI for the curve, and store for now
-    CIs <- append(CIs, round(contrast.index(smooth.fit, log.e.offset[test-1]), 2))
+    # To estimate the starting point of the search for the CI we pass a straight line regression of the data
+
+    straight.line <- lm(y ~ x, data.frame(x=film.data$He+log.e.offset[test-1], y=film.data[,test]))
+    CIs <- append(CIs, round(contrast.index(smooth.fit, straight.line$coefficients, log.e.offset[test-1]), 2))
    
     # Add the current curve to the plot
     p <- p + 
@@ -100,72 +85,46 @@ plot.film.test <- function(film.data,
   print(p)
 }
 
-#print(contrast.index(d, 0.21))
 
-plot.film.test(min.delta, "Delta 100 4x5 XTOL 1:1 20˚C 10min Tray", "Exposure\nEseco SL-2", log.e.offset=c(0.21,0,0), df=5)
-
-# $x [1] 0.8383949 1.0010792 2.6281244
-
-
-
-
-
-plot.film <- function(film.data, title) {
+# Computes the Kodak-style CI Contrast Index
+contrast.index <- function(curve.model, straight.line.coeff, log.e.offset = 0) {
   
-  ggplot(film.data, aes(x=He)) + coord_equal() + 
-    scale_x_continuous("rel log E", breaks=seq(0,4,0.30), minor_breaks = seq(0, 4, 0.10)) + 
-    scale_y_continuous("Density", breaks=seq(0,3,0.30), minor_breaks = seq(0, 4, 0.10)) + 
-    geom_point(aes(y=Green1Dh-min(Green1Dh), colour="A")) + 
-    geom_smooth(aes(y=Green1Dh-min(Green1Dh), colour="A"), method="loess", se=FALSE) +
-    geom_point(aes(x=He+0.21, y=Green2Dh-min(Green2Dh), colour="B")) + 
-    geom_smooth(aes(x=He+0.21, y=Green2Dh-min(Green2Dh), colour="B"), method="loess", se=FALSE) +
-    geom_point(aes(y=Blue1Dh-min(Blue1Dh), colour="C")) + 
-    geom_smooth(aes(x=He, y=Blue1Dh-min(Blue1Dh), colour="C"), method="loess" , se=FALSE) +
-    # geom_line(aes(x=He, y=Blue1Dh, colour="D"), data=d1) +
-    scale_colour_discrete(name="Exposure\nEseco SL-2", 
-                        breaks = c("A", "B", "C", "D"),
-                        labels = c("Green x1", "Green x2", "Blue", "BlueSm")) +
-    ggtitle(title)
+  require(nleqslv)
+  
+  # Estimate a good starting point for the search for the CI equation condition, as nleqslv is sensitive to starting parameters.
+  # Use a straight-line regression of the curve points as a way to estimate the starting points, as it is known they have to be
+  # spaced at 0 - 0.2 - 2.2 distances in 2d space, assuming that the curve is unlikely to be steeper than gradient=1.
+  
+  # TODO: Automatically vary the starting point if search fails, and try again?
+  
+  roots <- nleqslv(x=starting.point, fn=ci.equation, curve.model=curve.model, log.e.offset=log.e.offset,
+                   control=list(maxit=300))
+  
+  CI.points <- data.frame(He=c(roots$x[2:3]) - log.e.offset)
+  CI.points$D <- predict(curve.model, CI.points[1])
+  
+  if(roots$termcd == 1 & CI.points$He > 0 & CI.points$D > 0)
+    return((CI.points$D[2] - CI.points$D[1]) / (CI.points$He[2] - CI.points$He[1]))
+  else {
+    print("Error: Cannot compute CI for some or all curves. Consider changing the 'starting.point' as described in the source file comment.")
+    return(NA)
+  }
 }
 
 
+# Helper function, represents the system of non-linear equations based on Pythagorean triangle relationship
+# that describes the conditions of the CI measurement in terms of distances along concentric arcs
+# Many thanks to David dpgoldenberg on APUG.org for pointing this out.
 
-
-
-
-
-
-
-
-
-
-
-
-prev.plot.film <- function() {
+ci.equation <- function(x, curve.model, log.e.offset = 0) {
   
+  D <- function(x) 
+    predict(curve.model, data.frame(He=c(x-log.e.offset)))[1]
   
-  d1 <- delta100.xtol11
-  d1$Blue1Dh <- d1$Blue1Dh - min(d1$Blue1Dh)
-  smoothBl <- loess(Blue1Dh ~ He, d1)
+  c(
+    F1 = (x[2] - x[1])^2 + D(x[2])^2 - 0.2^2,
+    F2 = (x[3] - x[2])^2 + (D(x[3]) - D(x[2]))^2 - 2^2,
+    F3 = (x[3] - x[1])^2 + D(x[3])^2 - 2.2^2
+  )
   
-  
-  d <- delta100.xtol11
-  
-  #require(reshape2)
-  #d <- melt(delta100.xtol11, id.vars="")
-  
-  ggplot(d, aes(x=He)) + coord_equal() + 
-    scale_x_continuous("rel log E", breaks=seq(0,4,0.30), minor_breaks = seq(0, 4, 0.10)) + 
-    scale_y_continuous("Density", breaks=seq(0,3,0.30), minor_breaks = seq(0, 4, 0.10)) + 
-    geom_point(aes(y=Green1Dh-min(Green1Dh), colour="A")) + 
-    geom_smooth(aes(y=Green1Dh-min(Green1Dh), se=FALSE, colour="A")) +
-    geom_point(aes(x=He+0.21, y=Green2Dh-min(Green2Dh), colour="B")) + 
-    geom_smooth(aes(x=He+0.21, y=Green2Dh-min(Green2Dh), se=FALSE, colour="B")) +
-    geom_point(aes(y=Blue1Dh-min(Blue1Dh), colour="C")) + 
-    geom_smooth(aes(y=Blue1Dh-min(Blue1Dh), se=FALSE, colour="C")) +
-    scale_colour_manual(name="Exposure\nEseco SL-2", 
-                        breaks = c("A", "B", "C"),
-                        labels = c("Green x1", "Green x2", "Blue"),
-                        values = c("green3", "forestgreen", "blue")) +
-    ggtitle("Delta 100 4x5 XTOL 1:1 20˚C 10min Tray")
 }
